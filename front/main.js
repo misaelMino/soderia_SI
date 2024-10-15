d8c36500dbdf98a4fa7b8033448b9a7202351f3d
misaelMino <misael.mino@protonmail.com>
2024-10-15 18:56:51 -0300

    ultimo antes de ir a caseira

--- front/main.js
+++ front/main.js
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
             navigate(this.getAttribute('data-nav'));
         });
     });
-    navigate('clientes');
+    navigate('clientesPedidos');
 });
 
 
